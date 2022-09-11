0e075e8000646aad0ebadf78db0c06c17cad6387
BellsForHer <arhall88@gmail.com>
2022-09-11 14:38:08 -0500

    Create objects-example.js

---
+++ 06_in-depth_javascriptObjects/objects-example.js
@@ -0,0 +1,16 @@
+
+
+let person = {
+    firstName:"John",
+    age:35,
+    favoriteIceCream: "strawberry",
+    vehicle: {
+        model: "Honda"
+    }
+}
+
+console.log(`My name is ${person.firstName}`);
+console.log(`My age is ${person.age}`);
+console.log(`My favorit type of ice cream is ${person.favoritIceCream}`);
+
+console.log(`I own a ${person.vehicle.model}`);
